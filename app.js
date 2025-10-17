4fee3276a4a699a30b9310cc0bde9e49e245697c
Hasan Kourani <hasankoura8@gmail.com>
2025-10-17 22:11:05 +0300

    Constraint

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 const form = document.getElementById("daForm");
+const subBtn = document.getElementById("subBtn");
+let canSubmit = true;
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+setInterval(
+  () =>
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
 
-  const data = {
-    name: document.getElementById("name").value,
-    id: document.getElementById("id").value,
-    class: document.getElementById("class").value,
-  };
+      if (!canSubmit) {
+        alert("Please wait 5 seconds before trying to submit again :)");
+      }
 
-  console.log(data);
+      canSubmit = false;
+      setTimeout(() => {
+        canSubmit = true;
+      }, 5000);
 
-  const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbyEFlKD_e3LimSakZnLLXFS9Gm6FUJ1AusP_mWllrSceiKcbFYHI24GImyCuJNcUGL7Sg/exec",
-    {
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  if (response.ok) {
-    alert("Data submitted successfully!");
-    form.reset();
-  } else {
-    alert("Failed to submit data.");
-  }
-});
+      const data = {
+        name: document.getElementById("name").value,
+        id: document.getElementById("id").value,
+        class: document.getElementById("class").value,
+      };
+
+      console.log(data);
+
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyEFlKD_e3LimSakZnLLXFS9Gm6FUJ1AusP_mWllrSceiKcbFYHI24GImyCuJNcUGL7Sg/exec",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        alert("Data submitted successfully!");
+        form.reset();
+      } else {
+        alert("Failed to submit data.");
+      }
+    }),
+  5000
+);
